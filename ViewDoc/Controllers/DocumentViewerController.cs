d4747674bf1545084b23a9bbe41a96b5c767905f
miha <miha@sowalabs.com>
2013-06-22 10:20:55 +0200

    Implemented ability to retrieve cached HTML document.

--- ViewDoc/Controllers/DocumentViewerController.cs
+++ ViewDoc/Controllers/DocumentViewerController.cs
@@ -11,8 +11,10 @@
  ***************************************************************************/
 
 using System;
+using System.IO;
 using System.Text;
 using System.Web.Mvc;
+using System.IO.Compression;
 using Latino;
 using Latino.Workflows.TextMining;
 
@@ -29,7 +31,7 @@ namespace DocumentViewer.Controllers
     */
     public class DocumentViewerController : Controller
     {
-        private bool CheckRequest(string docId, out string fullFileName)
+        private bool CheckRequest(string docId, out string fullFileName, bool htmlFile = false)
         {
             fullFileName = null;
             if (docId == null)
@@ -56,11 +58,24 @@ namespace DocumentViewer.Controllers
                 ViewBag.Details = "Document ID: " + docId;
                 return false;
             }
-            fullFileName = LUtils.GetConfigValue<string>("DataRoot").TrimEnd('\\') + "\\" + fileName;
-            if (!LUtils.VerifyFileNameOpen(fullFileName))
+            if (htmlFile) { fileName = fileName.Replace(".xml.gz", ".html.gz"); }
+            string[] dataRoots = LUtils.GetConfigValue<string>(htmlFile ? "HtmlRoot" : "DataRoot").Split(';');
+            bool success = false;
+            string fileNames = "";
+            foreach (string dataRoot in dataRoots)
+            {
+                fullFileName = dataRoot.TrimEnd('\\') + "\\" + fileName;
+                fileNames += fullFileName + ", ";
+                if (LUtils.VerifyFileNameOpen(fullFileName))
+                {
+                    success = true;
+                    break;
+                }
+            }
+            if (!success) 
             {
                 ViewBag.ErrorMessage = "Document file name invalid or file not found.";
-                ViewBag.Details = "Document file name: " + fullFileName;
+                ViewBag.Details = "Assumed document file names: " + fileNames.Substring(0, fileNames.Length - 2);
                 return false;
             }
             return true;
@@ -85,6 +100,27 @@ namespace DocumentViewer.Controllers
             ViewBag.ContentParam = content;
             // render
             return View("View");
+        }
+
+        public ActionResult Html(string docId)
+        {
+            string xmlFileName, htmlFileName;
+            if (!CheckRequest(docId, out xmlFileName) || !CheckRequest(docId, out htmlFileName, /*htmlFile=*/true))
+            {
+                return View("Error");
+            }
+            Document doc = new Document("", "");
+            doc.ReadXmlCompressed(xmlFileName);
+            string charSet = doc.Features.GetFeatureValue("charSet");
+            byte[] bytes;
+            using (FileStream stream = new FileStream(htmlFileName, FileMode.Open))
+            {
+                using (GZipStream gzStream = new GZipStream(stream, CompressionMode.Decompress))
+                {
+                    bytes = gzStream.ReadAllBytes(/*sizeLimit=*/0);
+                }
+            }
+            return File(bytes, "text/html; charset=" + charSet);
         }
 
         [ActionName("Redirect")]
--- ViewDoc/Properties/AssemblyInfo.cs
+++ ViewDoc/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ViewDoc")]
+[assembly: AssemblyTitle("Document Viewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ViewDoc")]
+[assembly: AssemblyProduct("Document Viewer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
